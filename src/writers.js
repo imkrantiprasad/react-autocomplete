624b57a94fd1b6a04aa95d45b102bccf19095518
krantiprasad12 <57789517+krantiprasad12@users.noreply.github.com>
2020-06-11 19:48:56 +0530

    updates

--- src/writers.js
+++ src/writers.js
@@ -3,4 +3,12 @@ Faiz Ahmad Faiz
 Rahat Indori
 Jaun Elia
 Tehzeeb Hafi
+Mirza Ghalib
+Gulzar
+Javed Akhtar
+Ada Jafri
+Firaq Gorakhpuri
+Sahil Ludhianvi
+Bashir Badr
+Majrooh Sultanpuri
 `.split('\n');
